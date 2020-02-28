9c08872cc7b7a73e5d17db8286e27f59f17a6262
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-28 02:56:59 +0100

    mas api key

--- server/newsApi.js
+++ server/newsApi.js
@@ -1,5 +1,5 @@
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('get_your_own_api_key_at_newsapi_com');
+const newsapi = new NewsAPI('get your own API key at https://newsapi.org/');
 
 // To query top headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
@@ -7,4 +7,4 @@ const newsapi = new NewsAPI('get_your_own_api_key_at_newsapi_com');
 
 const getCryptoHeadlines = (data) => newsapi.v2.topHeadlines(data);
 
-module.exports = {getCryptoHeadlines};
+module.exports = { getCryptoHeadlines };
