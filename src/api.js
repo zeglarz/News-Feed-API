63bb5d3afb9c9b56ad7fb4c0bc976979f64628ce
Konrad Rudnicki <zeglarz@gmail.com>
2020-06-28 09:43:56 +0200

    style reformat

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const HOST_URL = 'paste_your_backend_URL_hee';
+const HOST_URL = 'paste_your_backend_URL_here';
 
 Api = {
     getNews: (country, category, query) => fetch(`${HOST_URL}?q=${query}&country=${country}&category=${category}`).then((response) => response.json())
