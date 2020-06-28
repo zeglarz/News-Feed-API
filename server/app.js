16914258daf504379abcec15294112f22f7ed96c
Konrad Rudnicki <zeglarz@gmail.com>
2020-06-28 13:23:22 +0200

    final changes

--- server/app.js
+++ server/app.js
@@ -3,40 +3,40 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const newsApi = require('./newsApi');
-const _ = require('lodash');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const getNews = async (obj) => await newsApi.getCryptoHeadlines(obj);
 
 app.get('/', (req, res) => {
+
     let obj = {
         q: '',
         category: '',
         language: '',
         country: ''
     };
+
     obj.language = req.query.country === undefined ? 'us' : req.query.country;
     obj.country = req.query.country === undefined ? 'us' : req.query.country;
     obj.category = req.query.category;
     obj.q = req.query.q;
+
     getNews(obj)
         .then(data => {
             let cleanedData = data.articles.map((article) => {
                 if (article.urlToImage === null) {
-                    article.urlToImage = 'https://drogariaguarulhos.com.br/media/catalog/product/placeholder/default/notfound.png';
+                    article.urlToImage = 'https://insidelatinamerica.net/wp-content/uploads/2018/01/noImg_2.jpg';
                 }
                 if (article.description === null || !article.description || article.description === '') {
                     article.description = 'Click and see more';
                 }
-
                 return article;
             });
             res.json(cleanedData);
         });
 });
-
 
 app.get('/query', (req, res) => {
     let obj = {
@@ -50,6 +50,7 @@ app.get('/query', (req, res) => {
     obj.country = req.query.country === undefined ? 'us' : req.query.country;
     obj.category = req.query.category === undefined ? '' : req.query.category;
     obj.q = query;
+
     getNews(obj)
         .then(data => {
             let searchedTitle = data.articles.filter(r => {
@@ -65,17 +66,14 @@ app.get('/query', (req, res) => {
             let set = new Set(concat);
             let joinedResponse = [...set];
 
-
             return res.json(joinedResponse);
         });
 });
 
-
 const runServer = (port) => {
     console.log(`Server is running on port ${port}`);
     app.listen(port);
-
 };
 
-module.exports = { runServer, getNews }; //strukturyzacja, czyli przeciwieństwo destrukturyzacji
+module.exports = { runServer, getNews };
 
--- server/newsApi.js
+++ server/newsApi.js
@@ -4,7 +4,6 @@ const newsapi = new NewsAPI('paste your api key here');
 // To query top headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
 
-
 const getCryptoHeadlines = (data) => newsapi.v2.topHeadlines(data);
 
 module.exports = { getCryptoHeadlines };
